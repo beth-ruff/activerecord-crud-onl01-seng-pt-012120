33690427d0354e367e32fdec0827a2a8209cf6a1
Beth Ruff <beth.ruff2@gmail.com>
2020-04-05 02:20:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,6 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:1990]
-  end
 end
 
 def can_get_the_first_item_in_the_database
